d27eb7f3d0d68af8faf519033550a1001fd1a008
Bbnesheim <145981762+Bbnesheim@users.noreply.github.com>
2025-07-30 03:06:18 +0200

    feat(next): add page titles and descriptions

--- next/pages/about.tsx
+++ next/pages/about.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import Head from 'next/head';
 
 export default function About() {
   return (
-    <main>
-      <h1>Om oss</h1>
-      <p>Dette er en forenklet Next.js-versjon av B\u00e5ttilsyn.</p>
-    </main>
+    <>
+      <Head>
+        <title>B\u00e5ttilsyn Next.js - Om oss</title>
+        <meta
+          name="description"
+          content="Informasjon om B\u00e5ttilsyn Next.js"
+        />
+      </Head>
+      <main>
+        <h1>Om oss</h1>
+        <p>Dette er en forenklet Next.js-versjon av B\u00e5ttilsyn.</p>
+      </main>
+    </>
   );
 }
