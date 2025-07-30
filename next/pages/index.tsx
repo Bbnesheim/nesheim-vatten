c7437a7d19386f77fa17256f429e344523cb274d
Bbnesheim <145981762+Bbnesheim@users.noreply.github.com>
2025-07-30 12:59:43 +0200

    chore(next): add typed props

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,11 +1,15 @@
 import dynamic from 'next/dynamic';
 import React from 'react';
 import Head from 'next/head';
+import type { HeavyModuleProps } from '../components/HeavyModule';
 
-const HeavyModule = dynamic(() => import('../components/HeavyModule'), {
-  loading: () => <p>Loading module...</p>,
-  ssr: false,
-});
+const HeavyModule = dynamic<HeavyModuleProps>(
+  () => import('../components/HeavyModule'),
+  {
+    loading: () => <p>Loading module...</p>,
+    ssr: false,
+  }
+);
 
 export default function Home() {
   return (
@@ -19,7 +23,7 @@ export default function Home() {
       </Head>
       <main>
         <h1>B\u00e5ttilsyn Next.js</h1>
-        <HeavyModule />
+        <HeavyModule content="This module could load large libraries like charts or PDF viewers." />
       </main>
     </>
   );
